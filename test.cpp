07eb6908e5b4ac58b8de78cda70d532d7db11727
Shreyakarmakar-26 <shreyakarmakar385@gmail.com>
2025-09-21 14:59:20 +0600

    Initial commit

--- test.cpp
+++ test.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+int main() {
+    srand(time(0)); // seed random number
+    int number = rand() % 100 + 1; // random number between 1-100
+    int guess;
+    int tries = 0;
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "🎯 Welcome to the Number Guessing Game!\n";
+    cout << "I'm thinking of a number between 1 and 100.\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        tries++;
+
+        if (guess > number) {
+            cout << "Too high! Try again.\n";
+        } else if (guess < number) {
+            cout << "Too low! Try again.\n";
+        } else {
+            cout << "🎉 Correct! You guessed it in " << tries << " tries.\n";
+        }
+    } while (guess != number);
+
+    return 0;
 }
